a1d6a2a6bf48d3c7847d5a1aaaddfbe23bbcc19d
Sr-fer <ffernandezmartinez@danielcastelao.org>
2022-03-15 19:33:43 +0100

    fix #11

---
+++ ficha_victoria.js
@@ -0,0 +1,7 @@
+function ficha_victoria(){
+    let img = document.createElement('img');
+    img.src = "Imágenes/1157725.svg";
+    img.style.height = "100%"
+    img.style.width = "50%"
+    return (img)
+}
--- victoria.js
+++ victoria.js
@@ -2,58 +2,155 @@ function comprobarVictoria(ficha1, ficha2, ficha3, ficha4, ficha5, ficha6, ficha
     //De izquierda a derecha
     if (ficha1 == "X" && ficha2 == "X" && ficha3 == "X") {
         avisoVictoriaX()
+        document.getElementById("box1").innerHTML = ""
+        document.getElementById("box2").innerHTML = ""
+        document.getElementById("box3").innerHTML = ""
+        document.getElementById("box1").appendChild(ficha_victoria())
+        document.getElementById("box2").appendChild(ficha_victoria())
+        document.getElementById("box3").appendChild(ficha_victoria())
     } else if (ficha1 == "O" && ficha2 == "O" && ficha3 == "O") {
         avisoVictoriaO()
+        document.getElementById("box1").innerHTML = ""
+        document.getElementById("box2").innerHTML = ""
+        document.getElementById("box3").innerHTML = ""
+        document.getElementById("box1").appendChild(ficha_victoria())
+        document.getElementById("box2").appendChild(ficha_victoria())
+        document.getElementById("box3").appendChild(ficha_victoria())
     }
 
     if (ficha4 == "X" && ficha5 == "X" && ficha6 == "X") {
         avisoVictoriaX()
+        document.getElementById("box4").innerHTML = ""
+        document.getElementById("box5").innerHTML = ""
+        document.getElementById("box6").innerHTML = ""
+        document.getElementById("box4").appendChild(ficha_victoria())
+        document.getElementById("box5").appendChild(ficha_victoria())
+        document.getElementById("box6").appendChild(ficha_victoria())
     } else if (ficha4 == "O" && ficha5 == "O" && ficha6 == "O") {
         avisoVictoriaO()
+        document.getElementById("box4").innerHTML = ""
+        document.getElementById("box5").innerHTML = ""
+        document.getElementById("box6").innerHTML = ""
+        document.getElementById("box4").appendChild(ficha_victoria())
+        document.getElementById("box5").appendChild(ficha_victoria())
+        document.getElementById("box6").appendChild(ficha_victoria())
     }
 
     if (ficha7 == "X" && ficha8 == "X" && ficha9 == "X") {
         avisoVictoriaX()
+        document.getElementById("box7").innerHTML = ""
+        document.getElementById("box8").innerHTML = ""
+        document.getElementById("box9").innerHTML = ""
+        document.getElementById("box7").appendChild(ficha_victoria())
+        document.getElementById("box8").appendChild(ficha_victoria())
+        document.getElementById("box9").appendChild(ficha_victoria())
     } else if (ficha7 == "O" && ficha8 == "O" && ficha9 == "O") {
         avisoVictoriaO()
+        document.getElementById("box7").innerHTML = ""
+        document.getElementById("box8").innerHTML = ""
+        document.getElementById("box9").innerHTML = ""
+        document.getElementById("box7").appendChild(ficha_victoria())
+        document.getElementById("box8").appendChild(ficha_victoria())
+        document.getElementById("box9").appendChild(ficha_victoria())
     }
 
 
     //De arriba a abajo
     if (ficha1 == "X" && ficha4 == "X" && ficha7 == "X") {
         avisoVictoriaX()
+        document.getElementById("box1").innerHTML = ""
+        document.getElementById("box4").innerHTML = ""
+        document.getElementById("box7").innerHTML = ""
+        document.getElementById("box1").appendChild(ficha_victoria())
+        document.getElementById("box4").appendChild(ficha_victoria())
+        document.getElementById("box7").appendChild(ficha_victoria())
     } else if (ficha1 == "O" && ficha4 == "O" && ficha7 == "O") {
         avisoVictoriaO()
+        document.getElementById("box1").innerHTML = ""
+        document.getElementById("box4").innerHTML = ""
+        document.getElementById("box7").innerHTML = ""
+        document.getElementById("box1").appendChild(ficha_victoria())
+        document.getElementById("box4").appendChild(ficha_victoria())
+        document.getElementById("box7").appendChild(ficha_victoria())
     }
 
     if (ficha2 == "X" && ficha5 == "X" && ficha8 == "X") {
         avisoVictoriaX()
+        document.getElementById("box2").innerHTML = ""
+        document.getElementById("box5").innerHTML = ""
+        document.getElementById("box8").innerHTML = ""
+        document.getElementById("box2").appendChild(ficha_victoria())
+        document.getElementById("box5").appendChild(ficha_victoria())
+        document.getElementById("box8").appendChild(ficha_victoria())
     } else if (ficha2 == "O" && ficha5 == "O" && ficha8 == "O") {
         avisoVictoriaO()
+        document.getElementById("box2").innerHTML = ""
+        document.getElementById("box5").innerHTML = ""
+        document.getElementById("box8").innerHTML = ""
+        document.getElementById("box2").appendChild(ficha_victoria())
+        document.getElementById("box5").appendChild(ficha_victoria())
+        document.getElementById("box8").appendChild(ficha_victoria())
     }
 
     if (ficha3 == "X" && ficha6 == "X" && ficha9 == "X") {
         avisoVictoriaX()
+        document.getElementById("box3").innerHTML = ""
+        document.getElementById("box6").innerHTML = ""
+        document.getElementById("box9").innerHTML = ""
+        document.getElementById("box3").appendChild(ficha_victoria())
+        document.getElementById("box6").appendChild(ficha_victoria())
+        document.getElementById("box9").appendChild(ficha_victoria())
     } else if (ficha3 == "O" && ficha6 == "O" && ficha9 == "O") {
         avisoVictoriaO()
+        document.getElementById("box3").innerHTML = ""
+        document.getElementById("box6").innerHTML = ""
+        document.getElementById("box9").innerHTML = ""
+        document.getElementById("box3").appendChild(ficha_victoria())
+        document.getElementById("box6").appendChild(ficha_victoria())
+        document.getElementById("box9").appendChild(ficha_victoria())
     }
 
     //Diagonal
     if (ficha1 == "X" && ficha5 == "X" && ficha9 == "X") {
         avisoVictoriaX()
+        document.getElementById("box1").innerHTML = ""
+        document.getElementById("box5").innerHTML = ""
+        document.getElementById("box9").innerHTML = ""
+        document.getElementById("box1").appendChild(ficha_victoria())
+        document.getElementById("box5").appendChild(ficha_victoria())
+        document.getElementById("box9").appendChild(ficha_victoria())
     } else if (ficha1 == "O" && ficha5 == "O" && ficha9 == "O") {
         avisoVictoriaO()
+        document.getElementById("box1").innerHTML = ""
+        document.getElementById("box5").innerHTML = ""
+        document.getElementById("box9").innerHTML = ""
+        document.getElementById("box1").appendChild(ficha_victoria())
+        document.getElementById("box5").appendChild(ficha_victoria())
+        document.getElementById("box9").appendChild(ficha_victoria())
     }
 
     if (ficha3 == "X" && ficha5 == "X" && ficha7 == "X") {
         avisoVictoriaX()
+        document.getElementById("box3").innerHTML = ""
+        document.getElementById("box5").innerHTML = ""
+        document.getElementById("box7").innerHTML = ""
+        document.getElementById("box3").appendChild(ficha_victoria())
+        document.getElementById("box5").appendChild(ficha_victoria())
+        document.getElementById("box7").appendChild(ficha_victoria())
     } else if (ficha3 == "O" && ficha5 == "O" && ficha7 == "O") {
         avisoVictoriaO()
+        document.getElementById("box3").innerHTML = ""
+        document.getElementById("box5").innerHTML = ""
+        document.getElementById("box7").innerHTML = ""
+        document.getElementById("box3").appendChild(ficha_victoria())
+        document.getElementById("box5").appendChild(ficha_victoria())
+        document.getElementById("box7").appendChild(ficha_victoria())
     }
 }
 
 function avisoVictoriaX() {
     let temporizador = setTimeout(avisoX, 300, "Resultado")
+    $("#ganador").html("X")
 }
 
 function avisoX() {
@@ -62,7 +159,9 @@ function avisoX() {
 }
 
 function avisoVictoriaO() {
-    let temporizador = setTimeout(avisoO, 300, "Resultado")
+let temporizador = setTimeout(avisoO, 300, "Resultado")
+$("#ganador").html("O")
+
 }
 
 function avisoO() {
